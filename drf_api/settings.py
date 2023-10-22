68a0b61022165142a67ecaec7f53919d1f444d2f
JamieB92 <j.bonner6992@gmail.com>
2023-10-22 13:47:17 +0000

    fix broken cloudinary image - env.py was in wrong place

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
 if os.path.exists('env.py'):
     import env
+
 
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
+
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,7 +49,6 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-
     'profiles',
 ]
 
